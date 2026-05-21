5e314cb6b42ea458d38c9214b2692798e1458c2b
subhisha chintada <chintadasubhisha.23.csm@anits.edu.in>
2026-05-21 23:13:07 +0530

    Added crops API

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Home Route
 @app.get("/")
 def home():
     return {"message": "Krishi Mitra Backend Running"}
+
+# Crops Route
+@app.get("/crops")
+def get_crops():
+
+    crops = [
+        {
+            "crop_name": "Rice",
+            "price": 25,
+            "quantity": "100kg"
+        },
+        {
+            "crop_name": "Wheat",
+            "price": 30,
+            "quantity": "50kg"
+        }
+    ]
+
+    return {"available_crops": crops}
