3e008c3fa35ae2165c339ac5fbf540f1c4002951
Sahithi Andhavarapu <sahithi.andhavarapu12@gmail.com>
2026-05-22 16:22:14 +0530

    Added splash screen UI

---
+++ frontend/lib/screens/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.agriculture,
+              size: 100,
+              color: Colors.white,
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              'Krishi Mithra',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Text(
+              'Smart Farming Assistant',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white70,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
