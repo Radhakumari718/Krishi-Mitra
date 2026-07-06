3cf8c2a19bbcaeae1537aebd1d63f928cb561cb0
Harshitha Bodda <boddaharshitha2906@gmail.com>
2026-07-06 18:25:27 +0530

    short clear message about what you did

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 import 'utils/storage_service.dart';
 import 'utils/product_data.dart';
@@ -6,6 +7,14 @@ import 'screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://fgqveqzjjwryvhhwkiwi.supabase.co/rest/v1/',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZncXZlcXpqandyeXZoaHdraXdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMzc2MDQsImV4cCI6MjA5ODkxMzYwNH0.qIokgTxXjITifRCpB9gMfuao_OnyguwhDqfnTSGfzX4
+
+',
+  );
+
   try {
     final savedProducts = await StorageService.loadProducts();
     ProductData.loadProducts(savedProducts);
@@ -19,42 +28,31 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
 
       title: 'Krishi Mithra',
 
       theme: ThemeData(
-
         primarySwatch: Colors.green,
 
-        elevatedButtonTheme:
-            ElevatedButtonThemeData(
-
+        elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-
             backgroundColor: Colors.green,
             foregroundColor: Colors.white,
 
-            padding:
-                const EdgeInsets.symmetric(
+            padding: const EdgeInsets.symmetric(
               vertical: 14,
             ),
 
-            shape:
-                RoundedRectangleBorder(
-
-              borderRadius:
-                  BorderRadius.circular(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
                 12,
               ),
             ),
